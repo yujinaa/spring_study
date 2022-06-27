f725066cd6b0f909e8cdf2c8fa8444ed6307a9b6
yujinaa <olim920@naver.com>
2022-06-27 23:52:02 +0900

    코드 수정

--- di/src/main/java/com/care/di_test/MainClass.java
+++ di/src/main/java/com/care/di_test/MainClass.java
@@ -10,7 +10,7 @@ public class MainClass {
 		String path = "classpath:application_test.xml"; //classpath : src/main/resources의 경로를 의미
 		GenericXmlApplicationContext ctx = 
 				new GenericXmlApplicationContext(path); 
-		PrintBean pBean = ctx.getBean("printB", PrintBean.class); 
+		PrintBean pBean = ctx.getBean("printB", PrintBean.class); //해당 bean 가져오기
 
 		pBean.printString();
 	}
