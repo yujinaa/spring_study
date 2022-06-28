8327d2c8ff768b6d0ea17f0d8c1baaf4cd6441d9
yujinaa <olim920@naver.com>
2022-06-28 18:31:58 +0900

    @어노테이션 : @Controller, @RequestMapping

---
+++ mvc/src/main/java/com/care/root/MyController.java
@@ -0,0 +1,14 @@
+package com.care.root;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//@ : 어노테이션은 서버쪽에서 알아볼 수 있게 해주는 것
+@Controller //컨트롤러 어노테이션이 있어야지만 컨트롤러로 인식한다!!!
+public class MyController {
+	@RequestMapping(value="index")//index요청이 들어오면 이곳에서 받겠다 (web서버와 맵핑으로 url경로)
+	public String memberIndex() { //받은 메소드 실행
+		return "member/index";    //member폴더의 index를 요청  (views와 맵핑으로 파일 경로)
+	}
+
+}
