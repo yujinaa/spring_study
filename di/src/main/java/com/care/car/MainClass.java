ba4a7e5311d7847b863c2ad9bb81dbf51ec80216
yujinaa <olim920@naver.com>
2022-06-28 17:10:09 +0900

    변경 내용이 많을시 xml로 bean만들어 처리

--- di/src/main/java/com/care/car/MainClass.java
+++ di/src/main/java/com/care/car/MainClass.java
@@ -1,9 +1,16 @@
 package com.care.car;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		Car car = new Controller02(); //자바로 일반적인 controller 객체 만드는방법
+		//Car car = new Controller02(); //자바로 일반적인 controller 객체 만드는방법
+		String path = "classpath:applicationCAR.xml"; 
+		GenericXmlApplicationContext ctx = 
+				new GenericXmlApplicationContext(path); 
+		Car car = ctx.getBean("car", Car.class);
+		
 		System.out.println("메인에서 속력을내고 있습니다");
 		car.speed(); //controller에 있는 기능 사용으로 controller01의 오버라이딩에 의해 자식이 가지고 있는 speed실행 
 		
--- di/src/main/java/com/care/car/SubClass.java
+++ di/src/main/java/com/care/car/SubClass.java
@@ -1,10 +1,16 @@
 package com.care.car;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class SubClass {
 	public void subFunc() {
-		Car car = new Controller02(); 
+		//Car car = new Controller02(); 
+		String path = "classpath:applicationCAR.xml"; 
+		GenericXmlApplicationContext ctx = 
+				new GenericXmlApplicationContext(path); 
+		Car car = ctx.getBean("car", Car.class);
+		
 		System.out.println("서브에서 속력을내고 있습니다");
 		car.speed();
 	}
-
 }
