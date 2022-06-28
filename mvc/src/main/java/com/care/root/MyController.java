c0f44240e3825e38a8eb973ad58775e8fb2f4410
yujinaa <olim920@naver.com>
2022-06-28 18:52:49 +0900

    Model객체를 이용해서 키와 값을 저장하고, 저장값 가져오기

--- mvc/src/main/java/com/care/root/MyController.java
+++ mvc/src/main/java/com/care/root/MyController.java
@@ -1,6 +1,7 @@
 package com.care.root;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //@ : 어노테이션은 서버쪽에서 알아볼 수 있게 해주는 것
@@ -9,6 +10,13 @@ public class MyController {
 	@RequestMapping(value="index")//index요청이 들어오면 이곳에서 받겠다 (web서버와 맵핑으로 url경로)
 	public String memberIndex() { //받은 메소드 실행
 		return "member/index";    //member폴더의 index를 요청  (views와 맵핑으로 파일 경로)
+	}
+	
+	
+	@RequestMapping("logout") //값이 한개만 들어가면 value 생략가능
+	public String memberLogout(Model model) {//model 객체를 자동 주입
+		model.addAttribute("key","로그아웃 되었습니다");//model에 키와 값을 넣고 저장값 받아온다
+		return "member/logout"; 
 	}
 
 }
--- mvc/src/main/webapp/WEB-INF/views/member/logout.jsp
+++ mvc/src/main/webapp/WEB-INF/views/member/logout.jsp
@@ -6,7 +6,11 @@
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
-<body>
+<body>logout.jsp</br>
+<!-- 전부 동일 -->
+<h1>request : <%= request.getAttribute("key") %></h1> 
+<h1>requestScope : ${requestScope.key }</h1>
+<h1>request(el) : ${key }</h1>
 
 </body>
 </html>
