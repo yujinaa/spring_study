7e485d1870144f87c683b3abb8959fe3d681f5d8
yujinaa <olim920@naver.com>
2022-06-29 17:09:08 +0900

    어노테이션으로 빈 등록하기

--- mvc_02/src/main/java/com/care/root/MemberController.java
+++ mvc_02/src/main/java/com/care/root/MemberController.java
@@ -10,9 +10,9 @@ public class MemberController {
 
 	@RequestMapping("insert") //데이터를 추가한다는 가정
 	public String insert(Model model) {//해당 메소드 실행 후 service로 연결 (사용자로부터 데이터를 받는다)
-		ms = new MemberService();//new라는 연산으로 객체만들기
-		int result = ms.insert(); //MemberService로 넘어가서 insert만들기
-		model.addAttribute("result",result); //addAttribute로 결과값을 받아와서 model에 저장
+		//ms = new MemberService();//new라는 연산으로 객체만들기
+		//int result = ms.insert(); //MemberService로 넘어가서 insert만들기
+		model.addAttribute("result",0); //addAttribute로 결과값을 받아와서 model에 저장
 		return "di/index";//model에 저장된 값은 di의 index로 전달
 	}
 }
--- mvc_02/src/main/java/com/care/root/MemberDAO.java
+++ mvc_02/src/main/java/com/care/root/MemberDAO.java
@@ -1,6 +1,13 @@
 package com.care.root;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Repository
 public class MemberDAO {
+	public MemberDAO() {
+		System.out.println("DAO 생성자 실행");
+	}
 	public int insert() {//사용자로부터 넘겨받은 데이터를 db에 저장
 		System.out.println("db에 저장되었습니다");
 		return 1; //성공하면 1, 실패하면 0 반환
