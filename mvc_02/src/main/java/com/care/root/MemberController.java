f2c3d81c4b5fb301795dec7781d5e462dd292275
yujinaa <olim920@naver.com>
2022-06-29 17:17:45 +0900

    @Autowired로 빈 자동주입하기

--- mvc_02/src/main/java/com/care/root/MemberController.java
+++ mvc_02/src/main/java/com/care/root/MemberController.java
@@ -1,18 +1,21 @@
 package com.care.root;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller      //연결 기능만 한다
 public class MemberController {
+	@Autowired //빈 자동주입(자료형보고 빈주입)
 	MemberService ms; //controller는 service로 연결
 
 	@RequestMapping("insert") //데이터를 추가한다는 가정
 	public String insert(Model model) {//해당 메소드 실행 후 service로 연결 (사용자로부터 데이터를 받는다)
 		//ms = new MemberService();//new라는 연산으로 객체만들기
-		//int result = ms.insert(); //MemberService로 넘어가서 insert만들기
-		model.addAttribute("result",0); //addAttribute로 결과값을 받아와서 model에 저장
+		int result = ms.insert(); //MemberService로 넘어가서 insert만들기
+		System.out.println("ms : " +ms);
+		model.addAttribute("result",result); //addAttribute로 결과값을 받아와서 model에 저장
 		return "di/index";//model에 저장된 값은 di의 index로 전달
 	}
 }
--- mvc_02/src/main/java/com/care/root/MemberService.java
+++ mvc_02/src/main/java/com/care/root/MemberService.java
@@ -1,17 +1,20 @@
 package com.care.root;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 //service : 연산 기능 담당 
 @Service
 public class MemberService { 
+	@Autowired //빈 자동주입
 	MemberDAO dao;//service는 DAO를 호출한다
 	public MemberService() {//객체가 만들어졌기에 생성자호출
 		System.out.println("서비스 생성자 실행");
 	}
 	public int insert() { //Controller에서 데이터를 가지고 해당하는 메소드를 찾아간다
 		//dao = new MemberDAO(); 
+		System.out.println("dao : " +dao);
 		return dao.insert(); //DAO로 넘어가서 코드 작성 (사용자가 넘긴 값을 dao로 전달) 
 	}
 }
