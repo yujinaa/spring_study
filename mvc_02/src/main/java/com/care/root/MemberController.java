af3d8f64ee8995816c140ccf95cdc1f2ff97f7be
yujinaa <olim920@naver.com>
2022-06-29 20:29:09 +0900

    @Qualifier : 빈 여러개 있을 떄 그중 하나를 골라준다

--- mvc_02/src/main/java/com/care/root/MemberController.java
+++ mvc_02/src/main/java/com/care/root/MemberController.java
@@ -1,6 +1,7 @@
 package com.care.root;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller      //연결 기능만 한다
 public class MemberController {
 	@Autowired //빈 자동주입(자료형보고 빈주입)
+	@Qualifier("mmm")//빈여러개가 있을 떄 여러개 중 mmm을 넣어주겠다
 	MemberService ms; //controller는 service로 연결
 
 	@RequestMapping("insert") //데이터를 추가한다는 가정
@@ -15,7 +17,7 @@ public class MemberController {
 		//ms = new MemberService();//new라는 연산으로 객체만들기
 		int result = ms.insert(); //MemberService로 넘어가서 insert만들기
 		System.out.println("ms : " +ms);
-		model.addAttribute("result",result); //addAttribute로 결과값을 받아와서 model에 저장
+		model.addAttribute("result",result); //addAttribute로 결과값을 받아와서 model에 저장(jsp쪽으로 result이름으로 값0을 넘겨준다
 		return "di/index";//model에 저장된 값은 di의 index로 전달
 	}
 }
--- mvc_02/src/main/java/com/care/root/MemberService.java
+++ mvc_02/src/main/java/com/care/root/MemberService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 //service : 연산 기능 담당 
-@Service
+@Service("mmm")
 public class MemberService { 
 	@Autowired //빈 자동주입
 	MemberDAO dao;//service는 DAO를 호출한다
@@ -14,7 +14,7 @@ public class MemberService {
 	}
 	public int insert() { //Controller에서 데이터를 가지고 해당하는 메소드를 찾아간다
 		//dao = new MemberDAO(); 
-		System.out.println("dao : " +dao);
+		System.out.println("dao : "+dao);
 		return dao.insert(); //DAO로 넘어가서 코드 작성 (사용자가 넘긴 값을 dao로 전달) 
 	}
 }
