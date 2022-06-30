7a35ef45ed42e927c718bb01accaaf07022b681f
yujinaa <olim920@naver.com>
2022-06-30 22:47:56 +0900

    controller , service @autowired주입

--- mvc_practice/src/main/java/com/care/root/member/controller/MemberController.java
+++ mvc_practice/src/main/java/com/care/root/member/controller/MemberController.java
@@ -1,9 +1,13 @@
 package com.care.root.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.care.root.member.service.MemberService;
 
 @Controller
 public class MemberController {
+	@Autowired 	MemberService ms;
 	public MemberController() {
 		System.out.println("controller 생성자 실행"); //서버를 끄고 다시 실행했을 때 이 문구가 떠야 빈들이 만들어진것
 	}
--- mvc_practice/src/main/java/com/care/root/member/service/MemberServiceImpl.java
+++ mvc_practice/src/main/java/com/care/root/member/service/MemberServiceImpl.java
@@ -1,9 +1,13 @@
 package com.care.root.member.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.care.root.member.dao.MemberDAO;
 
 @Service
 public class MemberServiceImpl implements MemberService{ //상속받기
+	@Autowired 	MemberDAO dao;
 	public MemberServiceImpl() {
 		System.out.println("service 생성자 실행");//제대로 실행이 되는지 보기위해서 생성자를 만들어주는게 좋다
 	}
