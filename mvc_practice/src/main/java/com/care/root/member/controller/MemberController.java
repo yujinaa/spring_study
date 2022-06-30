54079690578a848f004b463ba714c4545f2cc3ea
yujinaa <olim920@naver.com>
2022-06-30 23:06:27 +0900

    해당 빈이 주입됐는지 test해보기

--- mvc_practice/src/main/java/com/care/root/member/controller/MemberController.java
+++ mvc_practice/src/main/java/com/care/root/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.care.root.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.care.root.member.service.MemberService;
 
@@ -10,6 +11,11 @@ public class MemberController {
 	@Autowired 	MemberService ms;
 	public MemberController() {
 		System.out.println("controller 생성자 실행"); //서버를 끄고 다시 실행했을 때 이 문구가 떠야 빈들이 만들어진것
+	}
+	@RequestMapping("test")//빈이 들어왔는지 ms를 출력해본다(서버를 끄고 다시 실행해 test페이지를 열면 콘솔에 출력돼는걸 확인
+	public void test() {
+		System.out.println("ms :" + ms);
+		ms.test();
 	}
 
 }
--- mvc_practice/src/main/java/com/care/root/member/service/MemberService.java
+++ mvc_practice/src/main/java/com/care/root/member/service/MemberService.java
@@ -2,5 +2,6 @@ package com.care.root.member.service;
 
 //interface는 @어노테이션 쓰지 않는다
 public interface MemberService {
+	public void test();
 
 }
