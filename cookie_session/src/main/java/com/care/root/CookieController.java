ea49104a95f9c65b2ca06a2291c0071aa6a4fa80
yujinaa <olim920@naver.com>
2022-07-06 16:52:58 +0900

    쿠키값 null-> 쿠키값 생성

--- cookie_session/src/main/java/com/care/root/CookieController.java
+++ cookie_session/src/main/java/com/care/root/CookieController.java
@@ -14,6 +14,7 @@ public class CookieController {
 	public String cookie(HttpServletResponse response, //사용자 응답용으로 사용
 							HttpServletRequest req,//쿠키값 얻어오기
 							@CookieValue(value = "myCookie", required=false) Cookie cookie) {//사용자에게 myCookie있다면 cookie에 넣겠다, 값이 없으면 required=false null값으로 처리한다
+		System.out.println("cookie : " + cookie);
 		//자바코드로 쿠키설정
 		Cookie cook = new Cookie("myCookie", "쿠키생성");
 		cook.setMaxAge(5);//시간설정
