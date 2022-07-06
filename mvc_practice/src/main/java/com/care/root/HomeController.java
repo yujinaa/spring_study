eab58b4d6f097d7226d8272f25dca2a480645079
yujinaa <olim920@naver.com>
2022-07-06 13:12:28 +0900

    redirect로 서버내부 위치이동 연습

--- mvc_practice/src/main/java/com/care/root/HomeController.java
+++ mvc_practice/src/main/java/com/care/root/HomeController.java
@@ -4,6 +4,8 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -16,24 +18,45 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
+
 		model.addAttribute("serverTime", formattedDate );
-		
+
 		return "home";
 	}
-	
+	@RequestMapping("index")
+	public String index() {
+		return "re/index";
+	}
+	@RequestMapping("result")
+	public String result(HttpServletRequest request) {
+		String id = request.getParameter("id");
+		if(id.equals("abc")) {//인증성공시
+			return "redirect:rsOK";//인증성공시 여기로
+		}else {
+			return "redirect:rsNO";//인증실패시 여기로
+
+		}
+	}
+	@RequestMapping("rsOK")
+	public String rsOK() {
+		return "re/rsOK";
+	}
+	@RequestMapping("rsNO")
+	public String rsNO() {
+		return "re/rsNO";
+	}
 }
---
+++ mvc_practice/src/main/webapp/WEB-INF/views/re/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>index.jsp<br>
+	<form action="result">
+		<input type="text" name="id"><br> <input type="submit"
+			value="전송">
+	</form>
+
+</body>
+</html>
