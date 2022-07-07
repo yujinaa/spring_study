72eed898192f43cc848ee87d6bc20454e4a40ec0
yujinaa <olim920@naver.com>
2022-07-07 14:02:14 +0900

    @ContextConfiguration(locations={classpath:TestMember.xml}) :객체 만들어졌는지 확인

--- mybatis_junit/src/test/java/com/care/root/TestMember.java
+++ mybatis_junit/src/test/java/com/care/root/TestMember.java
@@ -1,5 +1,22 @@
 package com.care.root;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.care.root.member.controller.MemberController;
+
+//테스트 동작때 무조건 있어야한다
+@RunWith(SpringRunner.class)//@RunWith :테스트 동작 , SpringRunner : 이걸로 동작시키겠다
+@ContextConfiguration(locations={"classpath:TestMember.xml"})
 public class TestMember {
+	@Autowired MemberController mc;  //autowired로 memberController 자동주입
+	
+	@Test
+	public void testMc() {
+		System.out.println("mc : " +mc);
+	}
 
 }
