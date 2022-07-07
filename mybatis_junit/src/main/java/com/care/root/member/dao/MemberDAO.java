6021bec20c2d62d4e361843d518068aac1513336
yujinaa <olim920@naver.com>
2022-07-07 15:17:55 +0900

    test파일에 src경로추가,dao객체 test

--- mybatis_junit/src/main/java/com/care/root/member/dao/MemberDAO.java
+++ mybatis_junit/src/main/java/com/care/root/member/dao/MemberDAO.java
@@ -1,5 +1,8 @@
 package com.care.root.member.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MemberDAO {
 
 }
--- mybatis_junit/src/test/java/com/care/root/TestMember.java
+++ mybatis_junit/src/test/java/com/care/root/TestMember.java
@@ -9,14 +9,17 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.care.root.member.controller.MemberController;
+import com.care.root.member.dao.MemberDAO;
 import com.care.root.member.service.MemberService;
 
 //테스트 동작때 무조건 있어야한다
 @RunWith(SpringRunner.class)//@RunWith :테스트 동작 , SpringRunner : 이걸로 동작시키겠다
-@ContextConfiguration(locations={"classpath:TestMember.xml"})
+@ContextConfiguration(locations={"classpath:TestMember.xml",
+									"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
 public class TestMember {
 	@Autowired MemberController mc;  //autowired로 memberController 자동주입
 	@Autowired MemberService ms;
+	@Autowired MemberDAO dao;
 	
 	@Test
 	public void testMc() {
@@ -27,5 +30,10 @@ public class TestMember {
 	public void testMs() {
 		System.out.println("ms : " +ms);
 		assertNotNull(ms);//null이 아니면 성공
+	}
+	@Test
+	public void testDao() {
+		System.out.println("dao : " +dao);
+		assertNotNull(dao);//null이 아니면 성공
 	}
 }
