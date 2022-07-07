e853dd294f6514541de914428319501f8f3110a2
yujinaa <olim920@naver.com>
2022-07-07 14:10:23 +0900

    service 객체도 만들어졌는지 test

--- mybatis_junit/src/test/java/com/care/root/TestMember.java
+++ mybatis_junit/src/test/java/com/care/root/TestMember.java
@@ -1,5 +1,7 @@
 package com.care.root;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,16 +9,23 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.care.root.member.controller.MemberController;
+import com.care.root.member.service.MemberService;
 
 //테스트 동작때 무조건 있어야한다
 @RunWith(SpringRunner.class)//@RunWith :테스트 동작 , SpringRunner : 이걸로 동작시키겠다
 @ContextConfiguration(locations={"classpath:TestMember.xml"})
 public class TestMember {
 	@Autowired MemberController mc;  //autowired로 memberController 자동주입
+	@Autowired MemberService ms;
 	
 	@Test
 	public void testMc() {
 		System.out.println("mc : " +mc);
+		assertNotNull(mc);//null이 아니면 성공
 	}
-
+	@Test
+	public void testMs() {
+		System.out.println("ms : " +ms);
+		assertNotNull(ms);//null이 아니면 성공
+	}
 }
