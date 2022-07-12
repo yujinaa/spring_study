66708ccbfcc18492334e488cfdcd4b103005c7a9
yujinaa <olim920@naver.com>
2022-07-12 23:54:47 +0900

    로그인없이 회원정보 클릭하면 경고창 띄우기

--- project_practice/src/main/java/com/care/root/member/interceptor/MemberInterceptor.java
+++ project_practice/src/main/java/com/care/root/member/interceptor/MemberInterceptor.java
@@ -1,5 +1,7 @@
 package com.care.root.member.interceptor;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -19,12 +21,17 @@ public class MemberInterceptor extends HandlerInterceptorAdapter
 		//컨트롤러 가기전에 미리 처리하기
 		HttpSession session = request.getSession();//session 얻어오기
 		if(session.getAttribute(LOGIN) == null) {//session이 없다면
-			response.sendRedirect("login");//login으로 가라
+			//response.sendRedirect("login");//login으로 가라
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.print("<script>alert('로그인 해주세요');"
+//					 + "location.href='/root/member/login';</script>");
+					 + "location.href='" +request.getContextPath() + "/member/login'; </script>");
 			return false;//false면 해당 sendRedirect가 들어간다
 		}
 		
 		System.out.println("index(컨트롤러) 실행 전 실행");
-		return true;//해당하는 사용자가 요청한 경로로 연결
+		return true;//해당하는 사용자가 요청한 경로로 연결, 여기서 인터셉터가 끝나면 멤버 컨트롤러가 실행된다
 	}
 
 	@Override
