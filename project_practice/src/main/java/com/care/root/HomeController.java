ffa9dff3f4736b339d993defc7c189542eb7744a
yujinaa <olim920@naver.com>
2022-07-13 22:53:00 +0900

    로그아웃시 쿠키도 사라지기

--- project_practice/src/main/java/com/care/root/HomeController.java
+++ project_practice/src/main/java/com/care/root/HomeController.java
@@ -4,10 +4,14 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,8 +41,10 @@ public class HomeController {
 		return "home";
 	}
 	@GetMapping("/index")//기본페이지이기 때문에 member컨트롤러가아닌 home컨트롤러에서 연결해준다 , /이렇게 할경우 바로 index가 뜸
-	public String index() {
+	public String index(HttpServletResponse response,
+			@CookieValue(value="loginCookie",required = false) Cookie loginCookie) {
 		System.out.println("컨트롤러의 index 실행");
+		System.out.println("index cookie : " + loginCookie);
 		return "index";
 	}
 	
