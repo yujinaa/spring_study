bb67864e120b566c708d304048ec42711c130d03
yujinaa <olim920@naver.com>
2022-07-14 15:03:41 +0900

    념겨주는 아이디,이름.이미지 넘어가는지 확인

--- file_email/src/main/java/com/care/root/controller/FileUploadController.java
+++ file_email/src/main/java/com/care/root/controller/FileUploadController.java
@@ -1,13 +1,29 @@
 package com.care.root.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @Controller
 public class FileUploadController {
 	@RequestMapping("form")
 	public String form() {
 		return "uploadForm";
+	}
+	@PostMapping("upload")
+	public String upload(MultipartHttpServletRequest mul) {
+		String id = mul.getParameter("id");
+		String name = mul.getParameter("name");
+		
+		//파일 가져오기
+		MultipartFile file = mul.getFile("file");
+		String originalName = file.getOriginalFilename();//실질적 파일 이름이 들어온다
+		System.out.println("id : " + id);
+		System.out.println("name : " + name);
+		System.out.println("originalName : " + originalName);
+		return "redirect:form";
 	}
 
 }
--- file_email/src/main/java/com/care/root/servicce/FileServiceImpl.java
+++ file_email/src/main/java/com/care/root/servicce/FileServiceImpl.java
@@ -1,5 +1,8 @@
 package com.care.root.servicce;
 
-public class FileServiceImpl {
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileServiceImpl implements FileService {
 
 }
