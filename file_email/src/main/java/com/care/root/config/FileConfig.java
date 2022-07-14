30a061afee5eb816de4600e57b7ebfd785c33a03
yujinaa <olim920@naver.com>
2022-07-14 14:39:37 +0900

    파일 업로드 빈만들기(방법2.xml에 만들기)

--- file_email/src/main/java/com/care/root/config/FileConfig.java
+++ file_email/src/main/java/com/care/root/config/FileConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @Configuration //설정하는 값,내용(설정하는 값을 빈으로 만들어준다, xml에서 만든 것을 자바 코드로 만드는 방식)
 public class FileConfig {
-	//파일 업로드 빈 만들기
+	//파일 업로드 빈 만들기(방법 1)
 	@Bean //파일 업로드할땐 무조건 빈을 만들어줘야 처리 가능
 	public CommonsMultipartResolver multipartResolver() {//CommonsMultipartResolver :자료형을 return 으로 돌려주겟다
 		CommonsMultipartResolver mr = new CommonsMultipartResolver();
