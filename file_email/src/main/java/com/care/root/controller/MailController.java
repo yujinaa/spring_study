2e9e3ae6ffb0ee4f8f9cf0a8e26396d6e030d2d2
yujinaa <olim920@naver.com>
2022-07-15 16:12:48 +0900

    이메일 설정하고, 보내기

--- file_email/src/main/java/com/care/root/controller/MailController.java
+++ file_email/src/main/java/com/care/root/controller/MailController.java
@@ -18,7 +18,7 @@ public class MailController {
 		
 		ms.sendMail("받을메일계정", "(제목)텍스트 메일","(내용)메일 보냅니다");
 		
-		response.setContentType("test/html;charset=utf-8");
+		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
 		out.print("메일이 전송되었습니다");
 	}
