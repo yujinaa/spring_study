b2e5e96c8157c2786f8f6892dd40106a092690e0
yujinaa <olim920@naver.com>
2022-07-17 00:22:30 +0900

    json : map으로도 dto를 받아올수있따

--- ajax/src/main/java/com/care/root/HomeController.java
+++ ajax/src/main/java/com/care/root/HomeController.java
@@ -3,12 +3,15 @@ package com.care.root;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,34 +24,46 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	@GetMapping("non_ajax")//동기 통신방식
-	public String nonAjax() {
-		System.out.println("non ajax 실행");
-		return "non_ajax";
-	}
-	
-	@GetMapping("ajax")//비동기 통신방식
-	public String ajax() {
-		System.out.println("ajax실행!!!");
-		return "ajax";
-	}
-	static int cnt = 0;
-	@GetMapping("ajax_result")
-	@ResponseBody//ajax쓸때 무조건 있어야하고, body쪽으로 응답하겠다는 의미
-	public String ajaxrEsult() {
-		return ++cnt + "";
-	}
+//	@GetMapping("non_ajax")//동기 통신방식
+//	public String nonAjax() {
+//		System.out.println("non ajax 실행");
+//		return "non_ajax";
+//	}
+//	
+//	@GetMapping("ajax")//비동기 통신방식
+//	public String ajax() {
+//		System.out.println("ajax실행!!!");
+//		return "ajax";
+//	}
+//	static int cnt = 0;
+//	@GetMapping("ajax_result")
+//	@ResponseBody//ajax쓸때 무조건 있어야하고, body쪽으로 응답하겠다는 의미
+//	public String ajaxrEsult() {
+//		return ++cnt + "";
+//	}
 	
 	@GetMapping("ajax01")
 	public String ajax01() {
 		return "ajax01";
+	}
+	@PostMapping(value = "ajax_result01", 
+					produces = "application/json; charset=utf-8")
+	@ResponseBody
+//	public InfoDTO ajaxResult01(@RequestBody InfoDTO dto) {
+	public Map jaxResult01(@RequestBody Map<String, Object> dto) {//map도 키와 값으로 이루어져 있기 때문에 처리 가능
+		System.out.println("이름 : " + dto.get("name"));
+		System.out.println("나이 : " + dto.get("age"));
+		System.out.println("주소 : " + dto.get("addr"));
+		return dto;
 	}
 
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+
+
+		@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
--- ajax/src/main/webapp/WEB-INF/views/ajax01.jsp
+++ ajax/src/main/webapp/WEB-INF/views/ajax01.jsp
@@ -10,14 +10,27 @@
 	function ajax() {
 		var n = $("#name").val(); //제이쿼리 문법
 		var a = document.getElemebtById("age").value;//자바스크립트 문법
-		var form = {age:a, name:n}//age에 사용자가 입력한 a를 가져오겠다
+		var addr = $("#addr").val()	;
+		var form = {age:a, name:n, addr:addr}//age에 사용자가 입력한 a를 가져오겠다
 		console.log(form)
+		$.ajax({
+			url : "ajax_result01", type: "POST",
+			//data : 서버로 보낼 데이터{age : 20, name : 홍길동}
+			data : JSON.stringify(form),//json문자열 형식으롭 바꾸겠다
+			dataType : "json",//서버로 돌려받는 값의 타입 지정
+			//서버로 보낼 데이터 설정
+			contentType : "application/json; charset=utf-8",//이 방식으로 보내겠다
+			success :function(result){
+				$("#label").text(result.name+":"+result.age)
+			},error: function(){alert("문제 발생")}
+		})
 	}
 </script>
 </head>
 <body>
-	<input type="text" id = "name"><br>
-	<input type="text" id = "age"><br>
+	<input type="text" id = "name" placeholder="이름"><br>
+	<input type="text" id = "age" placeholder="나이"><br>
+	<input type="text" id = "addr" placeholder="주소"><br>
 	<input type="button" onclick="ajax()" value="저장">
 	<hr> 결과 : <label id = "label"></label>
 </body>
