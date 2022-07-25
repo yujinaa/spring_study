7de76ec801f289f6a01084f605852410a36619dc
yujinaa <olim920@naver.com>
2022-07-25 16:50:31 +0900

    dto만들고, mapper도 추가

--- tx_aop/src/main/java/com/care/root/dto/SystemDTO.java
+++ tx_aop/src/main/java/com/care/root/dto/SystemDTO.java
@@ -1,5 +1,13 @@
 package com.care.root.dto;
 
 public class SystemDTO {
+	private int systemNum;
 
+	public int getSystemNum() {
+		return systemNum;
+	}
+
+	public void setSystemNum(int systemNum) {
+		this.systemNum = systemNum;
+	}
 }
--- tx_aop/src/main/java/com/care/root/dto/UserDTO.java
+++ tx_aop/src/main/java/com/care/root/dto/UserDTO.java
@@ -1,5 +1,13 @@
 package com.care.root.dto;
 
 public class UserDTO {
+	private int userNum;
 
+	public int getUserNum() {
+		return userNum;
+	}
+
+	public void setUserNum(int userNum) {
+		this.userNum = userNum;
+	}
 }
