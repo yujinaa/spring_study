6ecd45cdd596ce18940cdf040e39b62f4b5049ef
yujinaa <olim920@naver.com>
2022-07-25 22:25:35 +0900

    트랜젝션 사용해서시스템상 문제생기면 데이터 롤백시키기

--- tx_aop/src/main/java/com/care/root/service/TestServiceImpl.java
+++ tx_aop/src/main/java/com/care/root/service/TestServiceImpl.java
@@ -2,6 +2,8 @@ package com.care.root.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 import org.springframework.ui.Model;
 
 import com.care.root.mapper.TestMapper;
@@ -9,12 +11,15 @@ import com.care.root.mapper.TestMapper;
 @Service
 public class TestServiceImpl {
 	@Autowired TestMapper mapper;
+	
+	@Transactional
 	public void buy(Model model, int num) {
 		int[] result = {0,0};
 		try {
 			result[0] = mapper.userInsert(num);
 			result[1] = mapper.systemInsert(num);
 		} catch (Exception e) {
+			TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();//시스템에서 문제생기면 데이터를 원래대로 롤백하겠다
 			e.printStackTrace();
 		}
 		model.addAttribute("result",result);
